9f8e6e50b66a96b29c45e04ef4d739061f2f54b9
Varadan Kumar <varadankumar521@gmail.com>
2026-08-11 11:49:41 +0530

    Bump version to 1.21 (code 2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.varadan.hotspot"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
